85938e6ee060a9e79d65d66ce9fab1be4824e28f
HAVRM <hiroki.mine.0614.163@gmail.com>
2016-06-07 16:46:46 +0900

    0607_1646

--- class/Image_Engineering_F/canny.cpp
+++ class/Image_Engineering_F/canny.cpp
@@ -1,12 +1,10 @@
 #include <opencv2/opencv.hpp>
 
 int main(int arg,char *argv[]){
-  cv::Mat src_img=cv::imread("avr3_averaging.png",1);
+  cv::Mat src_img=cv::imread("image/avr6_threshold.png",CV_8UC1);
   cv::Mat chg_img;
   if(!src_img.data)return -1;
-  float dataA[][3]={{0.0,-1.0,0.0},{-1.0,5.0,-1.0},{0.0,-1.0,0.0}};
-  cv::Mat kernel(3,3,CV_32FC1,dataA);
-  cv::filter2D(src_img,chg_img,-1,kernel);
+  cv::Canny(src_img,chg_img,100,200);
   cv::namedWindow("img1");
   cv::namedWindow("img2");
   cv::imshow("img1",src_img);
--- class/Image_Engineering_F/sharpning.cpp
+++ class/Image_Engineering_F/sharpning.cpp
@@ -1,13 +1,12 @@
 #include <opencv2/opencv.hpp>
 
 int main(int arg,char *argv[]){
-  cv::Mat src_img=cv::imread("avr10_spn.png",1);
+  cv::Mat src_img=cv::imread("image/avr3_averaging.png",1);
   cv::Mat chg_img;
   if(!src_img.data)return -1;
-
-  cv::medianBlur(src_img,chg_img,3);
-  for(int i=0;i<2;i++)cv::medianBlur(chg_img,chg_img,3);
-
+  float dataA[][3]={{0.0,-1.0,0.0},{-1.0,5.0,-1.0},{0.0,-1.0,0.0}};
+  cv::Mat kernel(3,3,CV_32FC1,dataA);
+  cv::filter2D(src_img,chg_img,-1,kernel);
   cv::namedWindow("img1");
   cv::namedWindow("img2");
   cv::imshow("img1",src_img);
