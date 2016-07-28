ae4b93e1283c599fae4e2cf6b87b9a66ff299341
HAVRM <hiroki.mine.0614.163@gmail.com>
2016-07-28 23:24:07 +0900

    0728_2324_07

--- shell_script/my_lib/lib_sort.sh
+++ shell_script/my_lib/lib_sort.sh
@@ -9,3 +9,29 @@ then
 		return 0
 	fi
 fi
+cd new_book/
+PDFNAME=(`ls *.pdf 2> /dev/null`)
+for arg in ${PDFNAME[@]}
+do
+	arg1=${arg%_*}
+	PDFN=(`ls ${arg1}_*.pdf 2> /dev/null`)
+	if [ ${#PDFN} != 0 ]
+	then
+		cd ..
+		mkdir -p lib/${arg1}
+		for arg2 in ${PDFN[@]}
+		do
+			mv new_book/${arg2} lib/${arg1}/${arg2} 2> /dev/null
+		done
+		cd new_book/
+	fi
+done
+PDFNAME=(`ls *.pdf 2> /dev/null`)
+for arg in ${PDFNAME[@]}
+do
+	cd ..
+	mkdir -p lib/${arg%.*}
+	mv new_book/${arg} lib/${arg%.*}/${arg} 2> /dev/null
+	cd new_book/
+done
+cd ..
