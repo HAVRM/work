cc3628e81ee7c95cab1f23e48c4e70264bf57f55
HAVRM <hiroki.mine.0614.163@gmail.com>
2016-11-24 18:12:37 +0900

    1124_1812_30

--- shell_script/rpi/rpi_ubuntu14_setup.sh
+++ shell_script/rpi/rpi_ubuntu14_setup.sh
@@ -177,7 +177,7 @@ other_install()
 echo "---other_install---"
 cd ~
 update_upgrade
-echo $PASS | sudo -S apt-get -y install emacs24 imagemagick rar unrar pdftk screen git calibre v41-utils expect
+echo $PASS | sudo -S apt-get -y install emacs24 imagemagick unrar pdftk screen git calibre expect
 cd ~
 }
 
