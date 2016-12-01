4cdeef69f7dafd85621c066d95556dff4367a102
HAVRM <hiroki.mine.0614.163@gmail.com>
2016-12-01 17:56:28 +0900

    1201_1756_21

--- shell_script/home/shutdown_com.sh
+++ shell_script/home/shutdown_com.sh
@@ -16,4 +16,9 @@ rm -rf sd_res.txt
 . github_ctrl.sh 1>sd_res.txt 2>&1
 cd rpi2_u14_work
 . sub_git.sh >>/home/***/sd_res.txt 2>&1
+cd ~/AVR_mbed
+git add -A 1>sd_res.txt 2>&1
+DATA=`date '+%m%d_%H%M_%S'`
+git commit -m "${DATA}" 1>sd_res.txt 2>&1
+git push AVR_mbed master 1>sd_res.txt 2>&1
 cd $PLACEshutdown_com
