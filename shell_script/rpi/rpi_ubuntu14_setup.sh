a1aba2e60206ab3498581822537633af3606495a
HAVRM <hiroki.mine.0614.163@gmail.com>
2017-04-24 01:17:52 +0900

    0424_0117_25

--- shell_script/rpi/rpi_ubuntu14_setup.sh
+++ shell_script/rpi/rpi_ubuntu14_setup.sh
@@ -185,6 +185,10 @@ echo "---web_server_install---"
 cd ~
 update_upgrade
 echo $PASS | sudo -S apt-get -y install apache2
+echo ${PASS} | sudo -S sed -i -e "s/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI\n\tAddHandler cgi-script .cgi/g" /etc/apache2/apache2.conf
+cd /etc/apache2/mods-enabled
+echo ${PASS} | sudo -S ln -s ../mods-available/cgi.load .
+echo ${PASS} | sudo -S /usr/sbin/apachectl restart
 cd ~
 }
 
@@ -204,7 +208,7 @@ other_install()
 echo "---other_install---"
 cd ~
 update_upgrade
-echo $PASS | sudo -S apt-get -y install emacs24 imagemagick unrar pdftk screen git calibre expect
+echo $PASS | sudo -S apt-get -y install emacs24 imagemagick unrar pdftk screen git calibre expect curl
 cd ~
 }
 
